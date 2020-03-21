8bb3ee3d2de30914484ccc53aa9c15777285cbf6
Latif siddiq sunny <1505069.mls@ugrad.cse.buet.ac.bd>
2020-03-21 11:40:17 +0600

    Update main.dart

--- Spondon/covid19/lib/main.dart
+++ Spondon/covid19/lib/main.dart
@@ -5,7 +5,7 @@ import 'package:covid19/view/question.dart';
 import 'package:covid19/view/symtoms.dart';
 import 'package:covid19/view/welcome.dart';
 import 'package:flutter/material.dart';
-
+import 'package:covid19/view/statistics.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -23,6 +23,7 @@ class MyApp extends StatelessWidget {
         '/questions': (context) => QuestionScreen(),
         '/symptoms': (context) => Symptoms(),
         '/map': (context) => MapPage(),
+        '/statistics' : (context) => Statistics(),
         '/quarantine': (context) => Quarantine()
       },
     );
