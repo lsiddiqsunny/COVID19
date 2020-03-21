25b5e18cf898b770bee7895b2d3123f2b34e20dc
Latif siddiq sunny <1505069.mls@ugrad.cse.buet.ac.bd>
2020-03-21 12:37:55 +0600

    Updated file structure

--- Spondon/covid19/lib/view/predict.dart
+++ Spondon/covid19/lib/view/predict.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:share/share.dart';
 
 class PredictionPage extends StatelessWidget {
   final int disease;
   PredictionPage(this.disease);
   String getDecison() {
-    if (disease == -1) {
+    if(disease==-1){
       return "You may not have any disease.Stay safe!";
-    } else if (disease == 0) {
+    }
+    else if (disease == 0) {
       return "You may be cold fever. Contact Doctor";
     } else if (disease == 1) {
       return "You may have corona. Contact doctor ASAP.";
@@ -62,40 +62,6 @@ class PredictionPage extends StatelessWidget {
                                       color: Colors.blueAccent,
                                       fontSize: 17,
                                     )),
-                              ],
-                            )),
-                      ),
-                      RaisedButton(
-                        color: Colors.white,
-                        elevation: 0,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: new BorderRadius.circular(25.0),
-                            side:
-                                BorderSide(color: Colors.blueAccent, width: 2)),
-                        onPressed: () {
-                          final RenderBox box = context.findRenderObject();
-                          Share.share("text",
-                              subject: "subject",
-                              sharePositionOrigin:
-                                  box.localToGlobal(Offset.zero) & box.size);
-                          //Navigator.pop(context);
-                        },
-                        child: Container(
-                            height: 50,
-                            width: 80,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: <Widget>[
-                                Text('Share ',
-                                    style: TextStyle(
-                                      color: Colors.blueAccent,
-                                      fontSize: 17,
-                                    )),
-                                Icon(
-                                  Icons.share,
-                                  color: Colors.blueAccent,
-                                ),
                               ],
                             )),
                       )
